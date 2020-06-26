4b12ae328533148b05bd55e709b3aee8865b5390
Weijia Wang <9713184+wegank@users.noreply.github.com>
2020-06-26 16:53:02 +0200

    Changer l'exécutable en shell

--- src/main.ml
+++ src/main.ml
@@ -123,27 +123,6 @@ and apply_primitive (p : primitive) (args : sexpr list) : sexpr =
     | _ -> failwith "Syntax error :: Primitive :: wrong number of arguments"
     end
 
-(*
-let main () =
-  let rec tostring e =
-    match e with
-    | Atom (Int i) -> string_of_int i
-    | Atom (Bool b) -> string_of_bool b
-    | Atom (Str_ s) -> s
-    | Atom (List es) -> "[" ^ (String.concat "; " (List.map tostring es)) ^ "]"
-    | _ -> raise (Invalid_argument "Not simplifiable") in
-  let rec loop r =
-    let _ = print_string "scheme # " in
-    let str = read_line () in
-    let es = List.map (eval initial_env) (Parser.parse str) in
-    try (
-      let _ = print_string ("=> " ^String.concat "; " (List.map tostring es) ^ "\n") in 
-      loop (r + 1)) with Invalid_argument _ -> 
-      let _ = print_string ("Expression not simplifiable\n") in loop (r + 1) in 
-  loop 0 
-let () = main ()
-*)
-
 let example_1 : string = "(+ 1 2)"
 let example_2 : string = "(let (y (lambda (x) (+ x 10))) (y 2))"
 let example_3 : string = "((lambda (x) (let (a 10) (let (x 20) (+ x 1)))) 20000)"
